7bc68c6e44d7f93e363c6f9f879be26011b3ece1
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-03-23 21:51:06 +0000

    Submit data to api

--- src/RequestService.js
+++ src/RequestService.js
@@ -4,16 +4,18 @@ import axios from 'axios'
 export default function RequestService() {
     const [serviceRequestTypes, setsServiceRequestTypes] = useState([])
     const [serviceRequestCharges, setServiceRequestCharges] = useState([])
-    const [fetchError, setFetchError] = useState(false)
-    const [fetchSuccess, setFetchSuccess] = useState(false)
 
     // FORM INPUTS
     const [customer_name, setCustomerName] = useState('')
-    const [customer_phone_number, setCustomerPhoneNumber] = useState([''])
+    const [customer_phone_number, setCustomerPhoneNumber] = useState('')
     const [customer_address,setCustomerAddress] = useState('')
     const [closest_landmark,setClosestLandmark] = useState('')
     const [service_request_charge,setRequestCharge] = useState('')
-    const [service_request_type, setCoustomerRequestType] = useState('')
+    const [service_request_type, setRequestType] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [success, setSuccces] = useState('')
+    const [error, setError] = useState('')
+    
     // GET SERVICE
     const fetchServiceRequest = async () => {
         try {
@@ -21,9 +23,42 @@ export default function RequestService() {
             const serviceCharges = await axios.get('http://139.162.134.202:8000/api/service-request/get-service-delivery-charges')
             setServiceRequestCharges(serviceCharges.data.payload)
             setsServiceRequestTypes(serviceTypes.data.payload)
+            
         } catch (error) {
             console.log('fetch type error', error)
         }
+    }
+    
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        setIsLoading(true)
+        const data = {
+            "customer_name": customer_name,
+            "customer_phone_number": customer_phone_number,
+            "customer_address": customer_address,
+            "closest_landmark": closest_landmark,
+            "service_request_charge": service_request_charge,
+            "service_request_type": service_request_type
+        }
+
+       try {
+          const response = await axios.post('http://139.162.134.202:8000/api/service-request/post-service-request',data )
+           setIsLoading(false)
+           setSuccces(response.data.message)
+
+          // reset form inputs
+          setCustomerName('')
+          setCustomerPhoneNumber('')
+          setCustomerAddress('')
+          setClosestLandmark('')
+          setRequestCharge('') 
+          setRequestCharge('')  
+
+       } catch (error) {
+         setIsLoading(false)
+         setError('An Error occured try again')
+       }
+
     }
    //  MAKE API CALL HERE
    useEffect( () => {
@@ -37,43 +72,58 @@ export default function RequestService() {
                         <h2 className="pb-3">SERVICE REQUEST</h2>
                     </div>
                     <div className="col-11 col-sm-11 col-md-10 col-lg-9">
-                        <form>
+                        <form onSubmit={ handleSubmit }>
                             <div className="form-group">
                                 <label htmlFor="customer-name">Full Name </label>
                                 <input required type="text" name="customer-name" id="customer_name" 
-                                placeholder="Enter your full name" className="form-control" />
+                                    placeholder="Enter your full name" className="form-control"
+                                    onChange={e => setCustomerName(e.target.value)} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="customer_phone_number"> Phone Number </label>
                                 <input required type="tel" placeholder="Enter your phone number" 
-                                name="customer_phone_number" id="customer_phone_number" className="form-control" />
+                                    name="customer_phone_number" id="customer_phone_number" className="form-control" 
+                                    onChange={e => setCustomerPhoneNumber(e.target.value)}
+                                />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="customer_address">Enter Your Address </label>
                                 <input required type="address" 
-                                placeholder="Enter your address" name="customer_address" 
-                                id="customer_address" className="form-control" />
+                                    placeholder="Enter your address" name="customer_address" 
+                                    id="customer_address" className="form-control"
+                                    onChange={e => setCustomerAddress(e.target.value)} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="closest_landmark"> Closest Landark </label>
                                 <input required placeholder="Enter closest landmark" 
-                                type="text" name="closest_landmark" id="closest_landmark" 
-                                className="form-control" />
+                                    type="text" name="closest_landmark" id="closest_landmark" 
+                                    className="form-control" 
+                                    onChange={e => setClosestLandmark(e.target.value)}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="service_request_type">Select Service Request Type </label>
-                                <select required className="form-control" id="serivice_request_type">
+                                <select  className="form-control" id="serivice_request_type"
+                                    onChange={e => setRequestType(e.target.value)} required='required'>
                                     {serviceRequestTypes.map( type => ( <option key={type.service_id} value={type.service_id}>{type.service_name}</option>))}
                                 </select>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="service_request_charge">Select Service Request Charge</label>
-                                <select required className="form-control" name="serivice_request_charge">
+                                <select className="form-control" name="serivice_request_charge"
+                                    onChange={e => setRequestCharge(e.target.value)} required="required" >
                                 {serviceRequestCharges.map( type => ( <option key={type.service_delivery_charge_id} value={type.service_delivery_charge_id}>{type.service_delivery_charge_name} {`(GH₵:${type.service_delivery_charge})`}</option>))}
                                 </select>
                             </div>
+                            {success && <div class="alert alert-success" role="alert">
+                                {success}
+                            </div>}
+                            {error && <div class="alert alert-alert" role="alert">
+                                {error}
+                            </div>}
                             <div className="modal-footer">
-                                <button type="submit" className="btn btn-primary submit">Submit</button>
+                                {!isLoading &&  <button  type="submit" className="btn btn-primary submit">Submit</button>}
+                                {isLoading && <button className="btn btn-primary submit" type="button" disabled>Loading...
+                                </button>}
                             </div>
                         </form>  
                     </div>
