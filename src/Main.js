1f536a57057cee5b9c0050a7b3f7153def2883f7
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-04-02 20:29:18 +0000

    Updated

--- src/Main.js
+++ src/Main.js
@@ -13,14 +13,14 @@ export default function Main() {
         <main id="main_container"> 
            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
+                    <div className="carousel-item active" data-interval="1000">
                         <img className="" src={imageONe} alt="first image" />
                     </div>
-                    <div className="carousel-item ">
+                    <div className="carousel-item " data-interval="2000">
                         <img className=""  src={imageThree} alt="first image" />
                     </div>
-                    <div className="carousel-item ">
-                        <img className="" src={imageThree} alt="first image" />
+                    <div className="carousel-item " data-interval="3000">
+                        <img className="" src={imageTwo} alt="first image" />
                     </div>
                 </div>
                 <div className="sidebar_content">
@@ -31,6 +31,50 @@ export default function Main() {
                     <p>Please Login with <br /> your registered mobile number </p>
                     <input type="number" min="0" maxLength="10" placeholder="Number" /> <button>Login</button>
                 </div>
+           </div>
+           <div className="card__container__wraper">
+            <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-5">
+                        <div className="card_inside">
+                            <div className="title-img">
+                                <h2>Get a HVAC expert in 3 steps</h2>
+                                <p>Provide personal detetails</p>
+                                <p>Choose preferred time </p>
+                                <p>Manage your bookings</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-5">
+                    <div className="card_inside">
+                            <div className="title-img">
+                                <h2>Service Request</h2>
+                            </div>
+                            <div className="description">
+                                <div><p>For maintenance and breakdown of products
+                                    
+                                    </p></div>
+                                <div className="mt-4 mb-3 text-right">
+                                <p><a className="book-now" href="/customer/service_request" >Book Now</a></p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-5">
+                    <div className="card_inside">
+                            <div className="title-img">
+                            <h2>My Bookings</h2>
+                            </div>
+                            <div className="description">
+                                <div>
+                                     <p>Manage your bookings</p>
+                                </div>
+                                <div className="mt-4 mb-3 text-right"><p className="link_wrapper"><a className="book-now" href="/customer/bookings" >Booking List</a></p></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+            </div>
            </div>
            {/* <div className='main__card__wrapper'>
                 <div className=" main__card">
